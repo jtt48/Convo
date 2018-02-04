084d8fc1d97c3d9d9d1463f7b7fc0dece518d12c
Zylo <jtt48@nau.edu>
2018-02-04 08:34:08 -0700

    Actual first commit
    Somehow pointed at the wrong directory.

---
+++ yaml.js
@@ -0,0 +1,75 @@
+
+var yaml = `
+#------- Intents ------------
+#-20 -> -16
+hate:
+- To deceive you
+- To hurt you as much as possible
+- To make you doubt yourself
+- To make you doubt your beliefs
+- To get you to give them a reason to hurt you
+- To get you to sign your own death warrant
+
+#-15 -> -6
+dislike:
+- To get you to grow away from them
+- To get you to not talk to someone
+- To get you to not talk about something
+- To get you to not talk to them
+- To get you to go away
+- To get you to do something dangerous for them
+
+#-5 -> 5
+neutral:
+- To get you to open up
+- To get you to tell the truth
+- To get you to explain yourself
+- To get you to talk about something they enjoy
+- To get you to talk about something you hate
+- To get you to talk about a recent event
+- To get you to talk about your past
+- To get you to examine something they have
+- To get you to guess about the future
+- To get you to talk about someone else
+- To get you to go with them somewhere
+- To get you to make a judgment
+- To get you to talk to someone else
+- To get you to do something for them
+
+
+#6 -> 15
+like:
+- To get you to grow closer to them
+- To ask for your advice about a problem with a relationship
+- To ask for your advice about a problem
+- To ask if you need help
+- To get you to do something important for them
+
+
+
+#16 -> 20
+love:
+- To get you to flirt with them
+- To get you to kiss them
+- To get you to spend more time with them
+- To get you to do something meaningful for them
+- To get you to let them help you
+
+#------ Tactics -------------
+
+tactics:
+- By arguing
+- By joking
+- By beging
+- By stating how it is
+- By bargaining
+- By beating around the bush
+- By lieing
+- By telling it to you straight
+- By playing to your nature
+- By playing to your beliefs
+- By playing to your past
+
+
+
+`;
